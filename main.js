dfcadeb2ba0e31c7f97956819fe3271f317b01a9
WebDeveloper <101199860+JavaScriptWebb@users.noreply.github.com>
2022-05-20 17:20:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 for (let x = 0; x <10; x++){   
-// Multiplicatin num as "x".  
+// Multiplication num as "x".  
     for (let i = 0; i <10; i++){ 
       // "i" times for multiply
     console.log((x+1) + " x " + (i+1) + " = " + ((x+1)*(i+1)))}
