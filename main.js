e88714da369fbc1ad48b64a47b6d18401e8ae3a1
WebDeveloper <101199860+JavaScriptWebb@users.noreply.github.com>
2022-05-20 17:19:33 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+for (let x = 0; x <10; x++){   
+// Multiplicatin num as "x".  
+    for (let i = 0; i <10; i++){ 
+      // "i" times for multiply
+    console.log((x+1) + " x " + (i+1) + " = " + ((x+1)*(i+1)))}
+    }
+
+// For multiplication 1 to 10
